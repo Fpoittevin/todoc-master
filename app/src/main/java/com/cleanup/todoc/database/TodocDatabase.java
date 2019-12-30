8cc91938c9a9db853e9bcd4a83c251fcd1a3a95b
Fpoittevin <dev@francoispoittevin.Fr>
2019-12-30 16:46:03 +0100

    creation of TodocDatabase

--- app/src/main/java/com/cleanup/todoc/database/TodocDatabase.java
+++ app/src/main/java/com/cleanup/todoc/database/TodocDatabase.java
@@ -1,6 +1,32 @@
 package com.cleanup.todoc.database;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
+import com.cleanup.todoc.database.Dao.TaskDao;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+@Database(entities = {Task.class, Project.class}, version = 1, exportSchema = false)
 public abstract class TodocDatabase extends RoomDatabase {
+
+    private static volatile TodocDatabase INSTANCE;
+
+    public abstract TaskDao taskDao();
+    public abstract Project projectDao();
+
+    public static TodocDatabase getInstance(Context context) {
+        if(INSTANCE == null) {
+            synchronized (TodocDatabase.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TodocDatabase.class, "TodocDatabase.db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
