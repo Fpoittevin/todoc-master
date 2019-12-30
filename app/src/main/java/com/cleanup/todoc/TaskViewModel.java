2de7825d0fe4524dbc9c22293743e5183fa5600a
Fpoittevin <dev@francoispoittevin.Fr>
2019-12-30 16:59:20 +0100

    creation of TaskViewModel

---
+++ app/src/main/java/com/cleanup/todoc/TaskViewModel.java
@@ -0,0 +1,49 @@
+package com.cleanup.todoc;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+import com.cleanup.todoc.repository.ProjectRepository;
+import com.cleanup.todoc.repository.TaskRepository;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+public class TaskViewModel extends ViewModel {
+
+    private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+    private final Executor executor;
+
+    public TaskViewModel(TaskRepository taskRepository, ProjectRepository projectRepository, Executor executor) {
+        this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+        this.executor = executor;
+    }
+
+    // PROJECTS
+
+    public LiveData<List<Project>> getProjects() {
+        return projectRepository.getProjects();
+    }
+
+    // TASKS
+
+    public LiveData<List<Task>> getTasks() {
+        return taskRepository.getTasks();
+    }
+
+    public void createTask(Task task) {
+        executor.execute(() -> {
+            taskRepository.createTask(task);
+        });
+    }
+
+    public void deleteTask(Task task) {
+        executor.execute(() -> {
+            taskRepository.deleteTask(task);
+        });
+    }
+}
